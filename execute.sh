f19b4a2ec04b5c78388650d7be35dff6d5ccab04
Sameer <sameerpande12@github.com>
2019-02-17 21:28:27 +0530

    update

--- execute.sh
+++ execute.sh
@@ -1,4 +1,4 @@
 make
 python3 preprocess.py $1 $2
-./exe $2 conv1.txt conv2.txt fc1.txt fc2.txt
+./exe $2 Convolution_Layers/conv1.txt Convolution_Layers/conv2.txt Convolution_Layers/fc1.txt Convolution_Layers/fc2.txt
 rm $2
